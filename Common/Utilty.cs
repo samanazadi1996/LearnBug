6332ecf471817c258884ba63ed651318a0e8edbe
saman azadi <samanazadi1996@gmail.com>
2020-04-15 22:11:50 +0430

    Add New PersianDateTime

--- Common/Utilty.cs
+++ Common/Utilty.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MD.PersianDateTime;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
@@ -11,79 +12,28 @@ using System.Web;
 public static class Utilty
 {
 
-    public static DateTime ToPersianDate(this DateTime dt)
+    public static string ToPersianDate(this DateTime dt, string format = "hh:mm  yyyy/MM/dd")
     {
-        PersianCalendar pc = new PersianCalendar();
-        int year = pc.GetYear(dt);
-        int month = pc.GetMonth(dt);
-        int day = pc.GetDayOfMonth(dt);
-        int hour = pc.GetHour(dt);
-        int min = pc.GetMinute(dt);
-        int sec = pc.GetSecond(dt);
-        return new DateTime(year, month, day, hour, min, sec);
+        return new PersianDateTime(dt).ToString(format);
     }
 
     public static DateTime ToMiladiDate(this DateTime dt)
     {
-        PersianCalendar pc = new PersianCalendar();
-        return pc.ToDateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second,0);
 
+        return PersianDateTime.Parse(dt.ToString("yyyy/MM/dd hh:mm:ss")).ToDateTime();
     }
 
-    public static string ToPrice(this object dec)
+    public static string ToPrice(this double dec)
     {
-        string Src = dec.ToString();
-        Src = Src.Replace(".0000", "");
-        if (!Src.Contains("."))
-        {
-            Src = Src + ".00";
-        }
-        string[] price = Src.Split('.');
-        if (price[1].Length >= 2)
-        {
-            price[1] = price[1].Substring(0, 2);
-            price[1] = price[1].Replace("00", "");
-        }
-
-        string Temp = null;
-        int i = 0;
-        if ((price[0].Length % 3) >= 1)
-        {
-            Temp = price[0].Substring(0, (price[0].Length % 3));
-            for (i = 0; i <= (price[0].Length / 3) - 1; i++)
-            {
-                Temp += "," + price[0].Substring((price[0].Length % 3) + (i * 3), 3);
-            }
-        }
-        else
-        {
-            for (i = 0; i <= (price[0].Length / 3) - 1; i++)
-            {
-                Temp += price[0].Substring((price[0].Length % 3) + (i * 3), 3) + ",";
-            }
-            Temp = Temp.Substring(0, Temp.Length - 1);
-            // Temp = price(0)
-            //If price(1).Length > 0 Then
-            //    Return price(0) + "." + price(1)
-            //End If
-        }
-        if (price[1].Length > 0)
-        {
-            return Temp + "." + price[1];
-        }
-        else
-        {
-            return Temp;
-        }
+        return dec.ToString("#,##0");
     }
-  
+
     public static string Encrypt(this string str)
     {
         byte[] encData_byte = new byte[str.Length];
         encData_byte = System.Text.Encoding.UTF8.GetBytes(str);
         return Convert.ToBase64String(encData_byte);
     }
-
     public static string Decrypt(this string str)
     {
         System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
