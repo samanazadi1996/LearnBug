faab86a9a63f2834a12ce6cea3754fe666e5e4c3
saman azadi <samanazadi1996@gmail.com>
2020-12-26 20:11:01 +0330

    add Authorization in hangfire and add WebFramework Project

--- LearnBug/App_Start/Startup.cs
+++ LearnBug/App_Start/Startup.cs
@@ -3,7 +3,7 @@ using Hangfire.SqlServer;
 using Microsoft.Owin;
 using NLog;
 using Owin;
-using System.IO;
+using WebFramework.Configurations;
 
 [assembly: OwinStartup(typeof(LearnBug.Startup))]
 
@@ -15,15 +15,8 @@ namespace LearnBug
 
         public void Configuration(IAppBuilder app)
         {
-            GlobalConfiguration.Configuration
-    .UseSqlServerStorage(@"data source=.;initial catalog=LearnBugDatabase;integrated security=True;");
-
-            app.UseHangfireDashboard();
-            app.UseHangfireServer();
-            //RecurringJob.AddOrUpdate(() =>
-            //File.Delete(System.Web.HttpContext.Current.Server.MapPath("~") + "logs/app-log.txt"), Cron.DayInterval(1));
-
-            //RecurringJob.AddOrUpdate(() => MailSender.SendMail("adasdasd", "sadasdasdsad", "samanazadi1996@gmail.com"), Cron.MinuteInterval(50));
+            GlobalConfiguration.Configuration.UseSqlServerStorage(@"data source=.;initial catalog=LearnBugDatabase;integrated security=True;");
+            app.AddHangfireServices();
         }
     }
 }
---
+++ WebFramework/Configurations/HanfireConfigurationExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Hangfire;
+using Hangfire.Dashboard;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFramework.Configurations
+{
+
+
+    public static class HanfireConfigurationExtensions
+    {
+        public static void AddHangfireServices(this IAppBuilder app)
+        {
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] { new MyAuthorizationFilter() }
+            });
+            app.UseHangfireDashboard();
+
+            app.UseHangfireServer();
+            //RecurringJob.AddOrUpdate(() =>
+            //File.Delete(System.Web.HttpContext.Current.Server.MapPath("~") + "logs/app-log.txt"), Cron.DayInterval(1));
+
+            //RecurringJob.AddOrUpdate(() => MailSender.SendMail("adasdasd", "sadasdasdsad", "samanazadi1996@gmail.com"), Cron.MinuteInterval(50));
+
+        }
+
+    }
+
+    public class MyAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            // In case you need an OWIN context, use the next line, `OwinContext` class
+            // is the part of the `Microsoft.Owin` package.
+            var owinContext = new OwinContext(context.GetOwinEnvironment());
+
+            // Allow all authenticated users to see the Dashboard (potentially dangerous).
+            return owinContext.Authentication.User.Identity.IsAuthenticated;
+        }
+    }
+
+}
